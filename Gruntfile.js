d5b25f1e4ec14f518b326f452d5b0c33bcaa5333
bsehl <b@ordinarystudios.com>
2015-05-11 16:40:42 -0400

    re-up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2015-05-11 using
+// Generated on 2015-04-24 using
 // generator-webapp 0.5.1
 'use strict';
 
@@ -152,9 +152,8 @@ module.exports = function (grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     sass: {
       options: {
-        sourceMap: true,
-        includePaths: ['bower_components']
-        },
+        loadPath: 'bower_components'
+      },
       dist: {
         files: [{
           expand: true,
